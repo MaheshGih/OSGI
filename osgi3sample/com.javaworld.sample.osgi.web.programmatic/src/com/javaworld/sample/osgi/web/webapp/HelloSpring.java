6f2d4b0206892a464104c378f3dd2c0ab084fdcb
MaheshGih <ramr2200@gmail.com>
2016-12-01 13:48:14 +0530

    chnge made

--- osgi3sample/com.javaworld.sample.osgi.web.programmatic/src/com/javaworld/sample/osgi/web/webapp/HelloSpring.java
+++ osgi3sample/com.javaworld.sample.osgi.web.programmatic/src/com/javaworld/sample/osgi/web/webapp/HelloSpring.java
@@ -3,10 +3,10 @@ package com.javaworld.sample.osgi.web.webapp;
 public class HelloSpring {
 
 	public void start() throws Exception {
-		System.out.println("Hello from HelloSpring");
+		System.out.println("Hello from HelloSpring Hi");
 	}
 	
 	public void stop() throws Exception{
-		System.out.println("Goodbye from HelloSpring ");
+		System.out.println("Goodbye from HelloSpring Hi");
 	}
 }
