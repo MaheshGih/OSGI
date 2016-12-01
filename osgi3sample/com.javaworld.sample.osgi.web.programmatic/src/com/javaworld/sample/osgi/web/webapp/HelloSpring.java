f10307163814ca3d432f7545d42502daef44c677
MaheshGih <ramr2200@gmail.com>
2016-12-01 12:52:41 +0530

    Osgi web application plugins
    Osgi web application plugins

---
+++ osgi3sample/com.javaworld.sample.osgi.web.programmatic/src/com/javaworld/sample/osgi/web/webapp/HelloSpring.java
@@ -0,0 +1,12 @@
+package com.javaworld.sample.osgi.web.webapp;
+
+public class HelloSpring {
+
+	public void start() throws Exception {
+		System.out.println("Hello from HelloSpring");
+	}
+	
+	public void stop() throws Exception{
+		System.out.println("Goodbye from HelloSpring");
+	}
+}
---
+++ osgi3sample/com.javaworld.sample.osgi.web.programmatic/src/com/javaworld/sample/osgi/web/webapp/HttpServiceTracker.java
@@ -0,0 +1,34 @@
+package com.javaworld.sample.osgi.web.webapp;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+import org.osgi.util.tracker.ServiceTracker;
+
+public class HttpServiceTracker extends ServiceTracker{
+
+	public HttpServiceTracker(BundleContext context) {
+		super(context, HttpService.class.getName(), null);
+	}
+	public Object addingService(ServiceReference reference) {
+		HttpService httpService = (HttpService) context.getService(reference);
+		System.out.println();
+		try {			
+			httpService.registerResources("/helloworld.html", "/helloworld.html", null);
+			httpService.registerServlet("/helloworld", new HelloWorldServlet(), null, null);
+			System.out.println("Programatic");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return httpService;
+	}		
+	
+	public void removedService(ServiceReference reference, Object service) {
+		HttpService httpService = (HttpService) service;
+		httpService.unregister("/helloworld.html"); 
+		httpService.unregister("/helloworld");
+		super.removedService(reference, service);
+	}
+
+}
